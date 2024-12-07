18293a98e34c93a140cee0be37f81be1dcd92fe4
viniciusestevamsilva <viniciusestevamsilvajf@gmail.com>
2024-12-06 21:35:33 -0300

    Atividade SQLite - talvez esteja funcionando

--- projeto_sqlite/main.py
+++ projeto_sqlite/main.py
@@ -1,5 +1,6 @@
 import os
 import sqlite3
+import time
 from crud import Exibir
 from crud import Adicionar
 from crud import Atualizar
@@ -11,22 +12,53 @@ conn = sqlite3.connect("C:/vinicius/sqlite/projeto_sqlite/aeroporto.db")
 while True:
     os.system('cls') 
     print('+-----------------+          +-------------------+')
-    print('+ MENU COMANDOS   +          +      TABELAS      +')
+    print('| MENU COMANDOS   |          |      TABELAS      |')
     print('+-----------------+          +-------------------+')
-    print('+ 1 - Exibir      +          +   companhia_aerea +')
-    print('+ 2 - Adicionar   +          +      passageiro   +')
-    print('+ 3 - Atualizar   +          +  terminal_viagens +')
-    print('+ 4 - Apagar      +          +       ticket      +')
-    print('+ 5 - Finalizar   +          +                   +')
+    print('| 1 - Exibir      |          |   companhia_aerea |')
+    print('| 2 - Adicionar   |          |      passageiro   |')
+    print('| 3 - Atualizar   |          |  terminal_viagens |')
+    print('| 4 - Apagar      |          |       ticket      |')
+    print('| 5 - Finalizar   |          |                   |')
     print('+-----------------+          +-------------------+')
 
     opcao = input('Opção escolhida: ')
 
 # ========================== FINALIZAÇÃO =============
     if opcao == '5':
+        for i in range(0,3):
+            os.system('cls')
+            print('___________________________________________')
+            print('|//////                                 12%|')
+            time.sleep(2)
+            os.system('cls')
+            print('___________________________________________')
+            print('|///////////                            22%|')
+            time.sleep(2)
+            os.system('cls')
+            print('___________________________________________')
+            print('|/////////////////                      40%|')
+            time.sleep(2)
+            os.system('cls')
+            print('___________________________________________')
+            print('|//////////////////////////             68%|')
+            time.sleep(2)
+            os.system('cls')
+            print('___________________________________________')
+            print('|///////////////////////////////        75%|')
+            time.sleep(2)
+            os.system('cls')
+            print('___________________________________________')
+            print('|////////////////////////////////////// 99%|')
+            time.sleep(2)
+            os.system('cls')
+            print('Error de carregamento... Recomeçando')
+            time.sleep(2)
+            os.system('cls') 
+
+        print('Carregamento concluido !')
         os.system('cls') 
         print('+--------------------------+')
-        print('+    Programa finalizado   +')
+        print('|    Programa finalizado   |')
         print('+--------------------------+')
         break
 
@@ -35,24 +67,38 @@ while True:
     elif opcao == '1':
         os.system('cls') 
         print('+---------------+')
-        print('+    Exibindo   +')
+        print('|    Exibindo   |')
         print('+---------------+')
         tabela = input('Digite o nome da tabela para exibir: ')
         
-        if tabela == '' and tabela == int: # Ve se a tabela não esta vazia 
+        if tabela == '' and tabela == int: # Ve se a tabela não esta vazia
             os.system('cls') 
             print('+---------------------------------+')
-            print('+ Opção inválida. Tente novamente.+')
+            print('| Opção inválida. Tente novamente.|')
             print('+---------------------------------+')
-            input('Pressione ENTER para voltar')
+            input('|   Pressione ENTER para voltar   |')
             
         else:
+            for i in range(0,3):
+                os.system('cls') 
+                print('Preparando recursos  @')
+                time.sleep(2)
+                os.system('cls') 
+                print('Preparando recursos  . @')
+                time.sleep(2)
+                os.system('cls') 
+                print('Preparando recursos  .. @')
+                time.sleep(2)
+                os.system('cls') 
+                print('Preparando recursos  ... @')
+                time.sleep(2)
+            
             os.system('cls')
             exibir = Exibir()
             exibir.SELECT(tabela)
-            print(f'+ Exibindo a tabela: {tabela}')
+            print(f'| Exibindo a tabela: {tabela}')
             print('+------------------------------------+')
-            input('+    Pressione ENTER para voltar     +')
+            input('|    Pressione ENTER para voltar     |')
 
 
 # ========================== ADICIONAR =============
@@ -60,7 +106,7 @@ while True:
         while True:  # Loop para continuar pedindo a adição
             os.system('cls') 
             print('+-------------------+')
-            print('+    Adicionando    +')
+            print('|    Adicionando    |')
             print('+-------------------+')
             tabela = input('Digite o nome da tabela para adicionar: ')
             
@@ -68,20 +114,21 @@ while True:
             if not tabela or not isinstance(tabela, str):
                 os.system('cls') 
                 print('+---------------------------------+')
-                print('+ Opção inválida. Tente novamente.+')
+                print('| Opção inválida. Tente novamente.|')
                 print('+---------------------------------+')
-                input('Pressione ENTER para voltar')
+                input('|   Pressione ENTER para voltar   |')
                 continue
             
             else:
                 adicionar = Adicionar()
                 adicionar.INSERT(tabela)
                 os.system('cls')
+                exibir = Exibir()
+                exibir.SELECT(tabela)
+                print('|    Registro Adicionado com sucesso!  |')
                 print('+--------------------------------------+')
-                print('+    Registro Adicionado com sucesso!  +')
-                print('+--------------------------------------+')
-                print('+        Deseja adicionar outro?       +')
-                opcao = input('- :')
+                print('|        Deseja adicionar outro?       |')
+                opcao = input('| - :')
             
                 if opcao == 'n':
                     break 
@@ -92,7 +139,7 @@ while True:
         while True: 
             os.system('cls') 
             print('+-------------------+')
-            print('+    Atualizando    +')
+            print('|    Atualizando    |')
             print('+-------------------+')
             tabela = input('Digite o nome da tabela para atualizar: ')
             
@@ -100,9 +147,9 @@ while True:
             if not tabela or not isinstance(tabela, str):
                 os.system('cls') 
                 print('+---------------------------------+')
-                print('+ Opção inválida. Tente novamente.+')
+                print('| Opção inválida. Tente novamente.|')
                 print('+---------------------------------+')
-                input('Pressione ENTER para voltar')
+                input('|   Pressione ENTER para voltar   |')
                 continue
 
             identificador = input('Digite o identificador (ex: cpf ou id): ')
@@ -110,9 +157,9 @@ while True:
             if not identificador or not isinstance(identificador, str):
                 os.system('cls') 
                 print('+---------------------------------+')
-                print('+ Opção inválida. Tente novamente.+')
+                print('| Opção inválida. Tente novamente.|')
                 print('+---------------------------------+')
-                input('Pressione ENTER para voltar')
+                input('|   Pressione ENTER para voltar   |')
                 continue
 
             valor = input(f'Digite o valor do {identificador} para atualizar: ')
@@ -120,18 +167,20 @@ while True:
             if not valor or not isinstance(valor, str):
                 os.system('cls') 
                 print('+---------------------------------+')
-                print('+ Opção inválida. Tente novamente.+')
+                print('| Opção inválida. Tente novamente.|')
                 print('+---------------------------------+')
-                input('Pressione ENTER para voltar')
+                input('|   Pressione ENTER para voltar   |')
                 continue
 
             atualizar = Atualizar()
             atualizar.UPDATE(tabela, identificador, valor)
+            
             os.system('cls')
+            exibir = Exibir()
+            exibir.SELECT(tabela)
+            print('|    Registro Atualizado com sucesso !  |')
             print('+---------------------------------------+')
-            print('+    Registro Atualizado com sucesso !  +')
-            print('+---------------------------------------+')
-            print('+        Deseja atualizar outro?        +')
+            print('|        Deseja atualizar outro?        |')
             opcao = input('- :')
             if opcao == 'n':
                 break 
@@ -178,7 +227,8 @@ while True:
             apagar = Apagar()
             apagar.DELETE(tabela, identificador, valor)
             os.system('cls')
-            print('+------------------------------------+')
+            exibir = Exibir()
+            exibir.SELECT(tabela)
             print('+    Registro apagado com sucesso !  +')
             print('+------------------------------------+')
             print('+        Deseja apagar outro?        +')
@@ -191,6 +241,6 @@ while True:
     else:
         os.system('cls') 
         print('+---------------------------------+')
-        print('+ Opção inválida. Tente novamente.+')
+        print('| Opção inválida. Tente novamente.|')
         print('+---------------------------------+')
-        input('Pressione ENTER para voltar')
+        input('|   Pressione ENTER para voltar   |')
