3325a85c04b05745c8ef3e80676b90fe0b90ca6b
viniciusestevamsilva <viniciusestevamsilvajf@gmail.com>
2025-01-23 19:17:11 -0300

    tentando entender

--- exercicio_sql/crud.py
+++ exercicio_sql/crud.py
@@ -7,23 +7,221 @@ conn = sqlite3.connect("C:/vinicius/sqlite/exercicio_sql/viagens.db")
 conn.execute("PRAGMA foreign_keys = ON;")
 cursor = conn.cursor()
 
-
-class exibir:
-    def SELECT(self, tabela):
-            
+class Exibir:
+     def SELECT(self, tabela):
+        
         conn = sqlite3.connect("C:/vinicius/sqlite/exercicio_sql/viagens.db")
         cursor = conn.cursor()
+
         
         cursor.execute(f'SELECT * FROM {tabela}')
         resultado = cursor.fetchall()
 
+       
         tabela_formatada = PrettyTable()
 
+       
         colunas = [descricao[0] for descricao in cursor.description]
         tabela_formatada.field_names = colunas
-            
+
+        
         for row in resultado:
             tabela_formatada.add_row(row)
 
         print(tabela_formatada) 
+        conn.close()
+
+
+class Adicionar:
+    
+    def INSERT(self, tabela):
+        conn = sqlite3.connect("C:/vinicius/sqlite/exercicio_sql/viagens.db")
+        conn.execute("PRAGMA foreign_keys = ON;")
+        cursor = conn.cursor()
+
+        # Verifica se a tabela foi fornecida corretamente
+        if not tabela or not isinstance(tabela, str):
+            os.system('cls') 
+            print('+---------------------------------+')
+            print('| Opção inválida. Tente novamente.|')
+            print('+---------------------------------+')
+            input('|   Pressione ENTER para voltar   |')
+            return
+
+        print('+---------------------------+')
+        print('|    Deseja adicionar um    |')
+        print('|         ou varios?        |')
+        print('| 1 - para um               |')
+        print('| 2 - para varios           |')
+        print('+---------------------------+')
+        
+        escolha = input('Deseja adicionar um ou vários registros? (1 para um, 2 para vários): ')
+        
+        if escolha == '1':
+            # Obter os dados com base na estrutura esperada para a tabela
+            if tabela == 'passageiro':
+                colunas = ['nome', 'CPf', 'data_de_nascimento', 'classe', 'assento']
+            elif tabela == 'minha_passagens':
+                colunas = ['id_passageiro', 'preco', 'duracao_viagem', 'data_viagem', 'origem_destino']
+            elif tabela == 'voos':
+                colunas = ['origem_destino', 'status_voo', 'passagens_disponiveis', 'preco_passagem', 'companhia_responsavel']
+            elif tabela == 'companhias':
+                colunas = ['nome_companhia', 'numero_voo', 'avaliacao', 'aviao', 'capacidade']
+            else:
+                print('Tabela desconhecida')
+                return
+
+            dados = input(f'Digite os dados para {", ".join(colunas)} (separados por vírgula): ').split(',')
+            
+            # Verifica se a quantidade de dados bate com a quantidade de colunas
+            if len(dados) != len(colunas):
+                os.system('cls') 
+                print('+---------------------------------+')
+                print('| A quantidade de dados não corresponde à quantidade de colunas.')
+                print('+---------------------------------+')
+                input('|   Pressione ENTER para voltar   |')
+                return
+
+            cursor.execute(f"INSERT INTO {tabela} ({', '.join(colunas)}) VALUES ({', '.join(['?'] * len(colunas))})", dados)
+            conn.commit()
+
+        elif escolha == '2':
+            dados_lista = []
+            while True:
+                if tabela == 'passageiro':
+                    colunas = ['nome', 'CPf', 'data_de_nascimento', 'classe', 'assento']
+                elif tabela == 'minha_passagens':
+                    colunas = ['id_passageiro', 'preco', 'duracao_viagem', 'data_viagem', 'origem_destino']
+                elif tabela == 'voos':
+                    colunas = ['origem_destino', 'status_voo', 'passagens_disponiveis', 'preco_passagem', 'companhia_responsavel']
+                elif tabela == 'companhias':
+                    colunas = ['nome_companhia', 'numero_voo', 'avaliacao', 'aviao', 'capacidade']
+                else:
+                    print('Tabela desconhecida')
+                    return
+
+                dados = input(f'Digite os dados do próximo registro para {", ".join(colunas)} (separados por vírgula): ')
+                print('Digite "parar" para finalizar')
+                
+                if dados.lower() == 'parar':
+                    break
+                
+                dados_lista.append(dados.split(','))
+
+            if not dados_lista:
+                os.system('cls') 
+                print('+---------------------------------+')
+                print('| Não há registros para adicionar.|')
+                print('+---------------------------------+')
+                input('|   Pressione ENTER para voltar   |')
+                return
+
+            cursor.executemany(f"INSERT INTO {tabela} ({', '.join(colunas)}) VALUES ({', '.join(['?'] * len(colunas))})", dados_lista)
+            conn.commit()
+
+        conn.close()
+
+class Atualizar:
+    
+    def UPDATE(self, tabela, identificador, valor):
+        
+        conn = sqlite3.connect("C:/vinicius/sqlite/exercicio_sql/viagens.db")
+        conn.execute("PRAGMA foreign_keys = ON;")
+        cursor = conn.cursor()
+        
+        # Verifica se a tabela é válida
+        if not tabela or not isinstance(tabela, str):
+            os.system('cls') 
+            print('+---------------------------------+')
+            print('| Opção inválida. Tente novamente.|')
+            print('+---------------------------------+')
+            input('|   Pressione ENTER para voltar   |')
+            return
+        
+        # Verifica se o identificador e valor são válidos
+        if not identificador or not isinstance(identificador, str):
+            os.system('cls') 
+            print('+---------------------------------+')
+            print('| Identificador inválido. Tente novamente.|')
+            print('+---------------------------------+')
+            input('|   Pressione ENTER para voltar   |')
+            return
+
+        if not valor or not isinstance(valor, str):
+            os.system('cls') 
+            print('+---------------------------------+')
+            print('| Valor inválido. Tente novamente.|')
+            print('+---------------------------------+')
+            input('|   Pressione ENTER para voltar   |')
+            return
+        
+        # Determina as colunas com base na tabela
+        if tabela == 'passageiro':
+            colunas = ['nome', 'CPf', 'data_de_nascimento', 'classe', 'assento']
+        elif tabela == 'minha_passagens':
+            colunas = ['id_passageiro', 'preco', 'duracao_viagem', 'data_viagem', 'origem_destino']
+        elif tabela == 'voos':
+            colunas = ['origem_destino', 'status_voo', 'passagens_disponiveis', 'preco_passagem', 'companhia_responsavel']
+        elif tabela == 'companhias':
+            colunas = ['nome_companhia', 'numero_voo', 'avaliacao', 'aviao', 'capacidade']
+        else:
+            print('Tabela desconhecida.')
+            return
+
+        # Solicita os dados para atualização
+        colunas_atualizar = input(f'Digite as colunas que deseja atualizar para {", ".join(colunas)} (separadas por vírgula): ').split(',')
+        dados = input(f'Digite os novos dados para {", ".join(colunas_atualizar)} (separados por vírgula): ').split(',')
+
+        # Verifica se o número de colunas e dados corresponde
+        if len(colunas_atualizar) != len(dados):
+            print('+----------------------------------------+')
+            print("| O número de colunas não corresponde ao número de dados fornecidos. |")
+            print('+----------------------------------------+')
+            input('Pressione ENTER para voltar.')
+            return
+        
+        # Atualiza os dados
+        for i in range(len(colunas_atualizar)):
+            coluna = colunas_atualizar[i].strip()
+            dado = dados[i].strip()  
+            cursor.execute(f"UPDATE {tabela} SET {coluna} = ? WHERE {identificador} = ?", (dado, valor))
+
+        conn.commit()
+        conn.close()
+
+class Apagar:
+    
+    def DELETE(self, tabela, identificador, valor):
+        conn = sqlite3.connect("C:/vinicius/sqlite/exercicio_sql/viagens.db")
+        conn.execute("PRAGMA foreign_keys = ON;")
+        cursor = conn.cursor()
+        
+        # Verifica se a tabela e os parâmetros são válidos
+        if not tabela or not isinstance(tabela, str):
+            os.system('cls') 
+            print('+---------------------------------+')
+            print('| Opção inválida. Tente novamente.|')
+            print('+---------------------------------+')
+            input('|   Pressione ENTER para voltar   |')
+            return
+
+        if not identificador or not isinstance(identificador, str):
+            os.system('cls') 
+            print('+---------------------------------+')
+            print('| Identificador inválido. Tente novamente.|')
+            print('+---------------------------------+')
+            input('|   Pressione ENTER para voltar   |')
+            return
+
+        if not valor or not isinstance(valor, str):
+            os.system('cls') 
+            print('+---------------------------------+')
+            print('| Valor inválido. Tente novamente.|')
+            print('+---------------------------------+')
+            input('|   Pressione ENTER para voltar   |')
+            return
+        
+        # Deleta o registro
+        cursor.execute(f"DELETE FROM {tabela} WHERE {identificador} = ?", (valor,))
+        conn.commit()
         conn.close()
--- exercicio_sql/main.py
+++ exercicio_sql/main.py
@@ -1,34 +1,144 @@
 import os
 import sqlite3
 import time
+from crud import Exibir
+from crud import Adicionar
+from crud import Atualizar
+from crud import Apagar
 
+# Conectando com banco de dados
 conn = sqlite3.connect("C:/vinicius/sqlite/exercicio_sql/viagens.db")
 conn.execute("PRAGMA foreign_keys = ON;")
-cursor = conn.cursor()
-
-tabelas = {
-    
-    '1':('passageiro',  ['nome','CPf','data_de_nascimento','classe','assento']),
-    
-    '2':('minha_passagens', ['id_passageiro','preco','duracao_viagem','data_viagem','origem_destino']),
-    
-    '3':('voos',    ['origem_destino','status_voo','passagens_disponiveis','preco_passagem','companhia_responsavel']),
-    
-    '4':('companhias',  ['nome_companhia','numero_voo','avaliacao','aviao','capacidade'])
-           
-           }
 
 while True:
-    
-    os.system('cls') 
-    print('+-----------------+')
+    os.system('cls')
+    print('+---------------------------+')
+    print('|  Passagens Trivago |')
+    print('+---------------------------+')
+    print('| 1 - Verificar Disponibilidade de Voos |')
+    print('| 2 - Comprar Passagem                 |')
+    print('| 3 - Alterar Dados de Passagem        |')
+    print('| 4 - Cancelar Passagem               |')
+    print('| 5 - Finalizar Compra                |')
+    print('+---------------------------+')
 
-    print('+-----------------+')
-    print('| 1 - Exibir      |')
-    print('| 2 - Adicionar   |')
-    print('| 3 - Atualizar   |')
-    print('| 4 - Apagar      |')
-    print('| 5 - Finalizar   |')
-    print('+-----------------+')
+    opcao = input('Escolha uma opção: ')
+    os.system('cls')
 
-    opcao = input('Opção escolhida: ')
+# ========================== FINALIZAÇÃO ============= 
+    if opcao == '5':
+        os.system('cls') 
+        print('+---------------------------+')
+        print('|    Compra finalizada!      |')
+        print('+---------------------------+')
+        print('|   Obrigado por utilizar   |')
+        print('|       nosso serviço!      |')
+        print('+---------------------------+')
+        
+        time.sleep(3)
+        os.system('cls')
+        break
+
+# ========================== VERIFICAR VOOS DISPONÍVEIS ============= 
+    elif opcao == '1':
+        os.system('cls')
+        print('+------------------------+')
+        print('|  Verificando Disponibilidade de Voos |')
+        print('+------------------------+')
+        tabela = 'voos'  # Aqui o código mostra a tabela de voos disponíveis
+        
+        exibir = Exibir()
+        exibir.SELECT(tabela)
+        
+        print('| Escolha um voo e continue para comprar |')
+        print('+---------------------------------------+')
+        input('|  Pressione ENTER para voltar          |')
+
+# ========================== COMPRAR PASSAGEM ============= 
+    elif opcao == '2':
+        while True:
+            os.system('cls')
+            print('+-----------------------+')
+            print('| Comprar Passagem      |')
+            print('+-----------------------+')
+            print('Disponibilidade de Voos: ')
+            tabela = 'voos'  # Mostra a tabela de voos disponíveis
+            exibir = Exibir()
+            exibir.SELECT(tabela)
+            
+            voo_id = input('Digite o ID do voo para comprar: ')
+            
+            if voo_id:
+                # O código aqui pode adicionar um registro na tabela de passagens
+                adicionar = Adicionar()
+                adicionar.INSERT('minha_passagens')  # Adiciona a passagem comprada na tabela de passagens
+                print('| Passagem comprada com sucesso! |')
+                exibir = Exibir()
+                exibir.SELECT(tabela)
+                
+                print('| Deseja comprar outra passagem? |')
+                print('| Sim(s) ou Não(n) |')
+                opcao = input('- :')
+                if opcao == 'n':
+                    break
+
+# ========================== ALTERAR DADOS DE PASSAGEM ============= 
+    elif opcao == '3':
+        while True:
+            os.system('cls')
+            print('+------------------------+')
+            print('| Alterar Dados de Passagem |')
+            print('+------------------------+')
+            tabela = 'minha_passagens'  # Mostra a tabela de passagens compradas
+            exibir = Exibir()
+            exibir.SELECT(tabela)
+            
+            # O código permite que o cliente altere dados da passagem comprada
+            identificador = input('Digite o identificador (ID) da passagem para alterar: ')
+            valor = input(f'Digite o novo valor para o identificador {identificador}: ')
+            
+            if identificador and valor:
+                atualizar = Atualizar()
+                atualizar.UPDATE('minha_passagens', 'id_passagem', identificador)
+                print('| Dados da passagem atualizados com sucesso! |')
+                exibir = Exibir()
+                exibir.SELECT(tabela)
+                
+                print('| Deseja alterar outra passagem? |')
+                print('| Sim(s) ou Não(n) |')
+                opcao = input('- :')
+                if opcao == 'n':
+                    break
+
+# ========================== CANCELAR PASSAGEM ============= 
+    elif opcao == '4':
+        while True:
+            os.system('cls')
+            print('+-----------------------+')
+            print('| Cancelar Passagem     |')
+            print('+-----------------------+')
+            tabela = 'minha_passagens'  # Mostra a tabela de passagens compradas
+            exibir = Exibir()
+            exibir.SELECT(tabela)
+            
+            identificador = input('Digite o identificador (ID) da passagem para cancelar: ')
+            if identificador:
+                apagar = Apagar()
+                apagar.DELETE('minha_passagens', 'id_passagem', identificador)  # Apaga a passagem
+                print('| Passagem cancelada com sucesso! |')
+                exibir = Exibir()
+                exibir.SELECT(tabela)
+                
+                print('| Deseja cancelar outra passagem? |')
+                print('| Sim(s) ou Não(n) |')
+                opcao = input('- :')
+                if opcao == 'n':
+                    break
+
+# ========================== OPÇÃO INVALIDA ============= 
+    else:
+        os.system('cls') 
+        print('+---------------------------------+')
+        print('| Opção inválida. Tente novamente.|')
+        print('+---------------------------------+')
+        input('|   Pressione ENTER para voltar   |')
